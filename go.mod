4b69be7f857cf0b9a7ed95edb0110b771e1a61a8
Bricktop <marcel.mueller1@rwth-aachen.de>
2020-08-17 13:25:43 +0200

    Add initial implementation to iterate on (#1)
    * Add initial implementation to iterate on
    
    * Make gosec happy
    
    * Add comment to explain gosec

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/giantswarm/REPOSITORY_NAME
+module github.com/giantswarm/prow-log-aggregator
 
 go 1.14
--- main.go
+++ main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+type logAggregator struct {
+	context    string
+	kubeconfig string
+	namespace  string
+}
+
+func (l logAggregator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var args []string
+	if l.namespace != "" {
+		args = append(args, []string{"-n", l.namespace}...)
+	}
+	if l.context != "" {
+		args = append(args, []string{"-c", l.context}...)
+	}
+	if l.kubeconfig != "" {
+		args = append(args, []string{"-k", l.kubeconfig}...)
+	}
+	runName := strings.TrimPrefix(r.URL.Path, "/")
+	args = append(args, []string{"pipelinerun", "logs", runName}...)
+
+	/*
+		Make gosec exception as we call the tekton binary directly.
+		Purpose of the call is to retrieve logs from tekton pipeline pods.
+		No disruptive operation is possible.
+		Reference issue for gosec exclusion: https://github.com/securego/gosec/issues/106
+	*/
+	// #nosec
+	cmd := exec.CommandContext(r.Context(), "tkn", args...)
+	cmd.Stdout = w
+	cmd.Stderr = w
+	err := cmd.Run()
+	if err != nil {
+		_, _ = fmt.Fprintf(w, "tkn failed: %s", err)
+	}
+}
+
+func main() {
+	var handler logAggregator
+	flag.StringVar(&handler.context, "context", "", "name of the kubeconfig context to use (default: kubectl config current-context)")
+	flag.StringVar(&handler.kubeconfig, "kubeconfig", "", "kubectl config file (default: $HOME/.kube/config)")
+	flag.StringVar(&handler.namespace, "namespace", "", "namespace to use (default: from $KUBECONFIG)")
+	flag.Parse()
+	http.Handle("/", handler)
+	s := &http.Server{Addr: ":8080"}
+	log.Fatal(s.ListenAndServe())
+}
