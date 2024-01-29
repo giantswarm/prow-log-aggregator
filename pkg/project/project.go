a1cd0c2aacedb889597bbaf1eeed962b29bb366c
taylorbot <dev@giantswarm.io>
2024-01-29 07:53:53 +0000

    Bump version to 0.3.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "prow-log-aggregator"
 	source      string = "https://github.com/giantswarm/prow-log-aggregator"
-	version            = "0.3.2"
+	version            = "0.3.3-dev"
 )
 
 func Description() string {
